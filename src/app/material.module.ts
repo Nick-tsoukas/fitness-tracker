dfcae24c07ecf87419da78259148c4592cd6a41d
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-03-16 13:35:41 -0500

    Adding sign up page ...

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule  } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
+
+
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+        MatCheckboxModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+        MatCheckboxModule
+    ]
 })
 export class MaterialModule {
 
